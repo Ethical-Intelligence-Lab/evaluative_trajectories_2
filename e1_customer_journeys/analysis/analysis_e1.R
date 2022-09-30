f491099a1b860cdc463ccc20570c935dd9ef8c95
akaanug <46591206+akaanug@users.noreply.github.com>
2022-09-30 14:55:16 +0300

    Adding e4

--- e1_customer_journeys/analysis/analysis_e1.R
+++ e1_customer_journeys/analysis/analysis_e1.R
@@ -60,8 +60,6 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #set working directo
                                          ##FUNCTIONS FOR PREPROCESSING##
 ##================================================================================================================
 
-data <- d_raw
-
 PerformExclusions <- function(data) {
     "
     Excludes participants if they do not finish the survey, finished it too quickly (under 120 seconds), 
